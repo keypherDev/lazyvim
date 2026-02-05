eb42e097d5f7009efbeadd9184ac9abe5b40833c
👾kpDev👾 <arianavro02@gmail.com>
2026-02-04 19:06:29 -0500

    chore: refine treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,17 +5,21 @@ return {
       opts.ensure_installed = opts.ensure_installed or {}
       local ensure = opts.ensure_installed
       if type(ensure) == "table" then
-        if not vim.tbl_contains(ensure, "html") then
-          table.insert(ensure, "html")
-        end
-        if not vim.tbl_contains(ensure, "css") then
-          table.insert(ensure, "css")
+        for _, lang in ipairs({ "html", "css" }) do
+          if not vim.tbl_contains(ensure, lang) then
+            table.insert(ensure, lang)
+          end
         end
       end
 
       opts.highlight = opts.highlight or {}
       opts.highlight.enable = true
       opts.highlight.additional_vim_regex_highlighting = opts.highlight.additional_vim_regex_highlighting or {}
+
+      opts.indent = vim.tbl_deep_extend("force", opts.indent or {}, {
+        enable = true,
+        disable = { "yaml" },
+      })
     end,
   },
 }
