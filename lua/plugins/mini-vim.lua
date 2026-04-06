e7dd0b32b54e62d99fe0aae55bfb096e98b31236
Jeison Varilla <arianavro02@gmail.com>
2026-04-06 11:31:34 -0500

    feat(minivim): add plugins mini-vim

---
+++ lua/plugins/mini-vim.lua
@@ -0,0 +1,16 @@
+return {
+  -- Rodea texto usando atajos simples para cambiar delimitadores
+  {
+    "nvim-mini/mini.surround",
+    version = false,
+    event = "VeryLazy",
+    opts = {},
+  },
+
+  -- Alterna entre una sola línea y bloques multilínea en estructuras de código
+  {
+    "nvim-mini/mini.splitjoin",
+    version = false,
+    opts = {},
+  },
+}
