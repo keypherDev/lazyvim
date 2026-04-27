f5e4799852bac1dc0386a246717ed0396f2814a9
Jeison Varilla <arianavro02@gmail.com>
2026-04-27 16:56:57 -0500

    Feat(neo-tree): show hidden files in file browser

---
+++ lua/plugins/neo-tree-show-hidden.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_hidden = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules",
+            ".git",
+          },
+          never_show = {
+            ".DS_Store",
+          },
+        },
+      },
+    },
+  },
+}
